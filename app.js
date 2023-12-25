64d1b5a3ce60fc225394c303cb9df94490c1446e
JackJosue517 <jackjosue517@gmail.com>
2023-12-25 18:38:28 +0000

    Define the ENTRY POINT

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express")
 
 const app = express()
 
-app.use((req, res) => {
+app.use("/", (req, res) => {
+  res.status(201)
+  res.json({ message: "This is the ENTRY POINT OF OUR BACKEND API!" })
+})
+
+app.use("/api/", (req, res) => {
   res.status(201)
   res.json({ message: "Your request is received with success!" })
 })
