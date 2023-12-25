43dee79d9b373f363e3d71ba2a9363373779cc04
JackJosue517 <jackjosue517@gmail.com>
2023-12-25 18:54:41 +0000

    Add the missed vercel.json file

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
 
-app.use("/", (req, res) => {
-  res.status(201)
-  res.json({ message: "This is the ENTRY POINT OF OUR BACKEND API!" })
+app.get('/home', (req, res) => {
+  res.status(200)
+  res.json({ message: 'This is the ENTRY POINT OF OUR BACKEND API!' })
 })
 
-app.use("/api/", (req, res) => {
+app.use('/api/', (req, res) => {
   res.status(201)
-  res.json({ message: "Your request is received with success!" })
+  res.json({ message: 'Your request is received with success!' })
 })
 
 module.exports = app
